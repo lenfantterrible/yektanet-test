00513112d82e65fdbde3ed1c96636624a7cd0e3d
lenfantterrible <mehdiat6@gmail.com>
2022-01-31 19:39:00 +0330

    Create Advertiser Class

---
+++ python/advertiser.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3 
+from baseadvertising import BaseAdvertising 
+
+class Advertiser(BaseAdvertising):
+
+    totalClicks = 0
+
+    def __init__(self, id, name):
+        super(id)
+        self._name = name 
+
+    def getName(self):
+        return self._name 
+
+    def setName(self, name):
+        self._name = name
+
+    @staticmethod
+    def help():
+        return "Advertiser\nID:\tID of Advertiser\nName:\tName of Advertiser\nClicks:\tNumber of clicks of this Advertiser\nViews: Number of views of this Advertiser"
+    
+    @staticmethod
+    def describeMe():
+        return "Advertiser Class Represents The Entity of Advertiser"
+
+    def incClicks(self):
+        Advertiser._totalClicks += 1
+        super().incClicks()
+
+    def getTotalClicks():
+        return Advertiser._totalClicks
+    
