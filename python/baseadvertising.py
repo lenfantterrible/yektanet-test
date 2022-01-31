2e41366f383c49958265267cc64c73c6c42ca69e
lenfantterrible <mehdiat6@gmail.com>
2022-01-31 19:38:47 +0330

    Created Base Class

---
+++ python/baseadvertising.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3 
+
+class BaseAdvertising():
+
+    def __init__(self, id) -> None:
+        self._id = id 
+
+    def getId(self):
+        return self._id 
+
+    def setId(self, id):
+        self._id = id
+
+    def getClicks(self):
+        return self._clicks 
+
+    def incClicks(self):
+        self._clicks += 1 
+
+    def getViews(self): 
+        return self._views 
+
+    def incViews(self):
+        self._views += 1
+
+    @staticmethod 
+    def describeMe():
+        return "Class BaseAdvertising"
